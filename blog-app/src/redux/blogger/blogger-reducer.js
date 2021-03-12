aee834029075b7963a1e18afe3ad8cf6d3937d71
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-03-12 13:51:56 +0700

    update logic

--- blog-app/src/redux/blogger/blogger-reducer.js
+++ blog-app/src/redux/blogger/blogger-reducer.js
@@ -15,6 +15,7 @@ const bloggerReducer =(state = INITIAL_STATE, action)=>{
     case actionTypes.SEARCH_BLOG :
       return {
         ...state,
+        detail : INITIAL_STATE.dataPost.find(post => post.title.toLowerCase() === action.payload.name.trim().toLowerCase()),
         dataPost : INITIAL_STATE.dataPost.filter(post => post.title.toLowerCase() === action.payload.name.trim().toLowerCase()),
       };
     case actionTypes.DETAIL_BLOG :
